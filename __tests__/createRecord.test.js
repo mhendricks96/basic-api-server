05c464f4e3b755d1a4e16689f205389b851609ee
michael <mhendricks96@yahoo.com>
2022-01-12 18:27:58 -0800

    finished testing connections through get methods of records. next is to write all the functions for the records router

--- __tests__/createRecord.test.js
+++ __tests__/createRecord.test.js
@@ -1,5 +1,30 @@
 'use strict';
 
-const server = require('../lib/server.js');
+const { app } = require('../lib/server.js');
+const { db } = require('../lib/models/recordModel');
 const supertest = require('supertest');
-const request = supertest(server.app);
+const request = supertest(app);
+
+beforeAll(async () => {
+  await db.sync();
+});
+afterAll(async () => {
+  await db.drop();
+});
+
+describe('Testing the records router', () => {
+  it('should read from records data', async () => {
+    const response = await request.get('/record');
+
+    expect(response.status).toEqual(200);
+    expect(response.body.count).toBeDefined();
+    expect(response.body.results).toBeDefined();
+  });
+  it('shoud read from records data', async () => {
+    const response = await request.get('/record/1');
+
+    expect(response.status).toEqual(200);
+    expect(response.body.count).toEqual(1);
+    expect(response.body.results).toBeDefined();
+  });
+});
--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 'use strict';
 
-const server = require('./lib/server.js');
-
+const { start } = require('./lib/server.js');
+const { db } = require('./lib/model');
 const PORT = process.env.PORT || 3000;
 
-server.start(PORT);
+db.sync()
+  .then(() => start(PORT))
+  .catch(err => console.log(err));
