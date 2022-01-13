37e2bee1c11233335cfe708a7cf1c5ba593f206f
michael <mhendricks96@yahoo.com>
2022-01-12 21:25:47 -0800

    can successfully post to record db and can GET all records or 1. next up is the put and delete methods

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const { start } = require('./lib/server.js');
-const { db } = require('./lib/model');
+const { db } = require('./lib/models/recordModel');
 const PORT = process.env.PORT || 3000;
 
 db.sync()
